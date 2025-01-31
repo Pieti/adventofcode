18d6f3463dd3a046c27fed886b3df4d587269771
Niko Pietikäinen <nikpie@proton.me>
2025-01-31 11:28:17 +0100

    2024 day 2 in go

---
+++ 2024/go/02/main1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	score := 0
+	for scanner.Scan() {
+		var report []int
+		parts := strings.Fields(scanner.Text())
+		for _, n := range parts {
+			level, _ := strconv.Atoi(n)
+			report = append(report, level)
+		}
+		if isSafe(report) {
+			score++
+		}
+	}
+
+	fmt.Println(score)
+}
+
+func isSafe(report []int) bool {
+	minDiff := 1
+	maxDiff := 3
+
+	if report[1]-report[0] < 0 {
+		minDiff = -3
+		maxDiff = -1
+	}
+
+	for i := 1; i < len(report); i++ {
+		diff := report[i] - report[i-1]
+		if diff < minDiff || diff > maxDiff {
+			return false
+		}
+	}
+	return true
+}
